3cc2a256d55a120d492faeff3ef452cda3254282
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2019-02-26 09:59:09 +0100

    Opdracht 1
    Eerste opdracht voor Front-end voor Designers

---
+++ opdracht1/Script/script.js
@@ -0,0 +1,36 @@
+function addToFavorite1() {
+
+    console.log(film1);
+    
+    var test = document.getElementById("favoFilm1");
+    
+    test.classList.toggle("active");
+    if(add1.innerHTML=="Remove")
+        add1.innerHTML = "Add";
+    else add1.innerHTML = "Remove";
+}
+
+
+var film1 = document.getElementById("favoFilm1");
+var add1 = document.getElementById("addFilm1");
+
+console.log(add1);
+add1.addEventListener('click', addToFavorite1);
+
+
+function addToFavorite2(){
+
+    console.log(film2);
+    
+    var test = document.getElementById("favoFilm2");
+    
+    test.classList.toggle("active");
+    if(add2.innerHTML=="Remove")
+        add2.innerHTML = "Add";
+    else add2.innerHTML = "Remove";
+}
+
+var film2 = document.getElementById("favoFilm2");
+var add2 = document.getElementById("addFilm2");
+
+add2.addEventListener('click', addToFavorite2);

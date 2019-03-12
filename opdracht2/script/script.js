9cd29b473c338e5219ae6e5c0527d38e0e187655
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2019-03-12 11:42:28 +0100

    Nav bar & Work beter neer gezet

--- opdracht2/script/script.js
+++ opdracht2/script/script.js
@@ -1,11 +1,14 @@
 var onScroll = document.querySelector('#bas');
 var myWork = document.querySelector('#work');
+var nav = document.querySelector('#navBar');
+var workBtn = document.querySelector('#goToWork')
 
 console.log("onscroll",onScroll);
 
 
 function move(){
   onScroll.classList.add("scrolling");
+  nav.classList.add("shadow");
   console.log("hoi", onScroll.offsetTop);
   console.log("myWork", myWork.offsetTop);
   console.log("window", window.scrollY);
@@ -14,6 +17,12 @@ function move(){
 
 window.addEventListener('scroll', move);
 
-if (offsetTop = 1280) {
-  myWork.classList.add("test")
+// if (offsetTop = 1280) {
+//   myWork.classList.add("test")
+// }
+
+function workTop(){
+  window.scrollY = 707.2000122070312;
 }
+
+workBtn.addEventListener('click', workTop);

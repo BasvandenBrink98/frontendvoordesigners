f2643293304f894075b285583450f4a475a89cff
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2019-03-18 23:14:09 +0100

    Verdere aanpassingen

--- opdracht2/script/script.js
+++ opdracht2/script/script.js
@@ -6,24 +6,55 @@ var workBtn = document.querySelector('#goToWork')
 console.log("onscroll",onScroll);
 
 
-function move(){
-  onScroll.classList.add("scrolling");
-  nav.classList.add("shadow");
-  console.log("hoi", onScroll.offsetTop);
-  console.log("myWork", myWork.offsetTop);
-  console.log("window", window.scrollY);
 
-}
-
-window.addEventListener('scroll', move);
-
-// if (offsetTop = 1280) {
-//   myWork.classList.add("test")
+// function work(){
+//   window.scrollTop = 1000;
+//   // console.log("testtest", window.scrollY)
 // }
+//
+// workBtn.addEventListener('click', work);
 
-function work(){
-  window.scrollTop = 1000;
-  console.log("testtest", window.scrollY)
+var scrollPos = window.scrollY;
+
+
+function scroll(){
+  if(window.scrollY != 0){
+    nav.classList.add("belowTop");
+    nav.classList.remove("topPage");
+    console.log(window.scrollY);
+
+  }
+  else{
+    nav.classList.add("topPage");
+    nav.classList.remove("belowTop");
+
+
+  }
 }
 
-workBtn.addEventListener('click', work);
+window.addEventListener('scroll', scroll);
+console.log(window.scrollY);
+
+
+
+
+// function addBas(){
+//   if(window.scrollY >= 288.79998779296875){
+//     var li = document.createElement("li");
+//     var bas = document.createTextNode("Bas van den Brink");
+//     li.appendChild(bas);
+//     navList.appendChild(li);
+//     bas.setAttribute("id","bas2");
+//     console.log("addBas");
+//   }
+//   else{
+//     // li.removeElement("li");
+//     // bas.removeTextNode("Bas van den Brink");
+//     // bas.removeAttribute("id","bas2");
+//   }
+// }
+//
+// window.addEventListener('scroll', addBas);
+//
+//
+//////     Hij bleef maar list items toevoegen terwijl er gescrollt werd, dus ivm tijd heb ik het anders op gelost     //////
